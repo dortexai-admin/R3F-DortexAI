1713ffb31cc6f4d1208001f897457a0d7c0795fc
khushalGrover <khushal15grover@gmail.com>
2024-10-15 00:17:01 +0530

    bugging commit

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,4 +1,5 @@
 import { Scroll } from "@react-three/drei";
+import content from "../constants/index";
 
 const Section = (props) => {
 	return (
@@ -22,75 +23,14 @@ export const Overlay = () => {
 	return (
 		<Scroll html>
 			<div className="w-screen">
-				<Section>
-					<h1>Welcome title here</h1>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ipsum odio itaque. Pariatur distinctio fugit praesentium dolorem adipisci unde quas.
-					</p>
-				</Section>
 
-				<Section right>
-					<h1>Our Mission</h1>
-					<p>
-						Our mission of Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, at quis quibusdam in voluptas culpa, aut suscipit cumque aperiam reiciendis fugiat consequatur eveniet sequi omnis quod distinctio debitis voluptatibus a ipsam porro! Animi adipisci ab eius ea voluptas corrupti quasi, porro dolorum cumque, molestiae quidem iure error perspiciatis asperiores ad?
-					</p>
-				</Section>
-				<Section>
-					<h1>Our Vision</h1>
-					<p>
-						Vision of an Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos aut cumque repudiandae praesentium quia accusantium dolorum nam quis perspiciatis, asperiores at accusamus alias vero pariatur neque inventore vel minima blanditiis rem, voluptatibus ex eveniet. A natus omnis quaerat voluptatum, alias recusandae, veniam ipsa exercitationem quos laborum et labore eum assumenda.
-					</p>
-				</Section>
+				{content.map((item, index) => (
+					<Section key={index} right={index % 2 === 0}>
+						<h1>{item.title}</h1>
+						<p>{item.description}</p>
+					</Section>
+				))}
 
-				<Section right>
-					<h1>Our Hightlights</h1>
-					<ul>
-						<li>some features</li>
-						<li>other features</li>
-						<li>one more feature </li>
-						<li>Last feature</li>
-					</ul>
-				</Section>
-				<Section>
-					<h1>Our Exports</h1>
-					<ol>
-						<li>
-							<ul>
-								<li>ABCD</li>
-								<li>
-									Lorem ipsum, dolor sit amet consectetur
-									adipisicing elit. Reprehenderit consectetur
-									sit ipsa?
-								</li>
-							</ul>
-						</li>
-
-						<li>
-							<ul>
-								<li>ABCD</li>
-								<li>
-									Lorem ipsum, dolor sit amet consectetur
-									adipisicing elit. Reprehenderit consectetur
-									sit ipsa?
-								</li>
-							</ul>
-						</li>
-
-						<li>
-							<ul>
-								<li>ABCD</li>
-								<li>
-									Lorem ipsum, dolor sit amet consectetur
-									adipisicing elit. Reprehenderit consectetur
-									sit ipsa?
-								</li>
-							</ul>
-						</li>
-
-						
-					</ol>
-				</Section>
-				
 			</div>
 		</Scroll>
 	);
---
+++ src/constants/index.js
@@ -0,0 +1,44 @@
+const content = [
+    {
+        title: "Welcome title here",
+        description: "Welcome description here lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.",
+    },
+    {
+        title: "Our Mission",
+        description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aut cumque repudiandae praesentium quia accusantium dolorum nam quis perspiciatis, asperiores at accusamus alias vero pariatur neque inventore vel minima blanditiis rem, voluptatibus ex eveniet. A natus omnis quaerat voluptatum, alias recusandae, veniam ipsa exercitationem quos laborum et labore eum assumenda.",
+    },
+    {
+        title: "Our Vision",
+        description: "Vision of an Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos aut cumque repudiandae praesentium quia accusantium dolorum nam quis perspiciatis, asperiores at accusamus alias vero pariatur neque inventore vel minima blanditiis rem, voluptatibus ex eveniet. A natus omnis quaerat voluptatum, alias recusandae, veniam ipsa exercitationem quos laborum et labore eum assumenda.",
+    },
+    {
+        title: "Our Highlights",
+        description: [
+            "some features",
+            "other features",
+            "one more feature",
+            "Last feature",
+        ],
+    },
+    {
+        title: "Our Exports",
+        description: [
+            {
+                title: "ABCD",
+                description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit consectetur sit ipsa?",
+            },
+            {
+                title: "ABCD",
+                description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit consectetur sit ipsa?",
+            },
+            {
+                title: "ABCD",
+                description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit consectetur sit ipsa?",
+            },
+        ],
+    },
+
+]
+
+
+export default content;
